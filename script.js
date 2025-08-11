e28b6b118c390efd023b94ad82a6104917ab4533
Your Name <you@example.com>
2025-08-11 10:32:07 +0200

    function delete

--- script.js
+++ script.js
@@ -98,9 +98,3 @@ function order() {
     dialog.close();
   });
 }
-
-document.getElementById("theme-toggle").addEventListener("click", function () {
-  const body = document.body;
-  const currentMode = body.getAttribute("darkmode");
-  body.setAttribute("darkmode", currentMode === "on" ? "off" : "on");
-});
